10521ab063177b364faaa4d8a1a9466c0877d8bd
ubselcuk <ubselcuk@gmail.com>
2025-08-21 08:53:10 +0300

    Update changelog for version 0.0.6 and enhance list extensions with generic support

--- lib/src/list_extensio.dart
+++ lib/src/list_extensio.dart
@@ -1,11 +1,13 @@
-extension NListExtensio on List? {
+extension NListExtensio<T> on List<T>? {
+  List<T> get orEmpty => this ?? <T>[];
+
   bool get isNullOrEmpty => this == null || this!.isEmpty;
   bool get isNotNullOrEmpty => this != null && this!.isNotEmpty;
-  bool get isNotEmpty => this != null && this!.isNotEmpty;
-  bool get isEmpty => this == null || this!.isEmpty;
-  List get orEmpty => this ?? [];
-  int get length => this?.length ?? 0;
-  dynamic get firstOrNull => this?.isNotEmpty == true ? this!.first : null;
-  dynamic get lastOrNull => this?.isNotEmpty == true ? this!.last : null;
-  List get reversed => this?.reversed.toList() ?? [];
+
+  int get lengthOrZero => this?.length ?? 0;
+
+  T? get firstOrNull => (this?.isNotEmpty ?? false) ? this!.first : null;
+  T? get lastOrNull => (this?.isNotEmpty ?? false) ? this!.last : null;
+
+  List<T> get reversedOrEmpty => this?.reversed.toList() ?? <T>[];
 }
